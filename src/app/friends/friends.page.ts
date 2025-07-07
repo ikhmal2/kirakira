253437681d7264dccb6acbb36fce556ce0923b3b
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-07-07 18:50:56 +0800

    feat: add list of friends section

--- src/app/friends/friends.page.ts
+++ src/app/friends/friends.page.ts
@@ -1,36 +1,80 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonItem,
+  IonAvatar,
+  IonLabel,
+  IonText,
+  IonButton,
+} from '@ionic/angular/standalone';
 import { HeaderComponent } from '../components/header/header.component';
 import { FriendsService } from '../services/friends.service';
-import { Friend, Friends } from '../services/interfaces';
-import { DebtStatusComponent } from "../components/debt-status/debt-status.component";
+import { Friend } from '../services/interfaces';
+import { DebtStatusComponent } from '../components/debt-status/debt-status.component';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.page.html',
   styleUrls: ['./friends.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, HeaderComponent, DebtStatusComponent]
+  imports: [
+    IonButton,
+    IonText,
+    IonLabel,
+    IonItem,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonAvatar,
+    CommonModule,
+    FormsModule,
+    HeaderComponent,
+    DebtStatusComponent,
+  ],
 })
 export class FriendsPage implements OnInit {
-  private friendsService = inject(FriendsService)
-  public friendsList: Friend[] = []
+  private friendsService = inject(FriendsService);
+  public friendsList: Friend[] = [];
+  public settledFriendsList: Friend[] = [];
+  public showSettledFriends = false;
 
   constructor() {
-    this.loadFriends()
+    this.loadFriends();
   }
 
   loadFriends() {
     this.friendsService.getFriendsList().subscribe({
       next: (res) => {
-        this.friendsList.push(...res.friends)
-      }
+        for (let i = 0; i < res.friends.length; i++) {
+          if (
+            res.friends[i].amountOwed === 0 &&
+            res.friends[i].debtAmount === 0
+          ) {
+            this.settledFriendsList.push(res.friends[i]);
+          } else {
+            this.friendsList.push(res.friends[i]);
+          }
+        }
+      },
     });
   }
 
-  ngOnInit() {
+  displaySettledFriends() {
+    this.showSettledFriends = !this.showSettledFriends;
+    if (this.showSettledFriends) {
+      document
+        .querySelector('#settled-friends')
+        ?.classList.add('visibility', 'collapse');
+    } else {
+      document
+        .querySelector('#settled-friends')
+        ?.classList.add('visibility', 'visible');
+    }
   }
 
+  ngOnInit() {}
 }
